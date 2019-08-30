39f3c6718536272b8af60d0ffde122ae156b7361
Pank <linkinpark02111@mail.ru>
2019-08-30 22:50:59 +0300

    init

---
+++ easy-ts.ru/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace easy_ts.ru.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
