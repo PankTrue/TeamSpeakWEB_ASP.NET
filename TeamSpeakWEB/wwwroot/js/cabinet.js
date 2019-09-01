e0f68e7f2f73c6e8be32a74f432a736c40b9410e
Pank <linkinpark02111@mail.ru>
2019-09-01 20:57:29 +0300

    refactoring

--- TeamSpeakWEB/wwwroot/js/cabinet.js
+++ TeamSpeakWEB/wwwroot/js/cabinet.js
@@ -1,9 +1,6 @@
-﻿
+﻿$(document).ready({
 
-
-$(document).ready({
-
-if((@ViewBag.current_user.auto_extension) && (($('#toggle_event_editing button').hasClass('locked_active')) || (('#toggle_event_editing button').hasClass('locked_active')))) {
+    if((UserAutoExtension) && (($('#toggle_event_editing button').hasClass('locked_active')) || (('#toggle_event_editing button').hasClass('locked_active')))) {
     $('#toggle_event_editing button').eq(0).toggleClass('locked_inactive locked_active btn-default btn-info');
     $('#toggle_event_editing button').eq(1).toggleClass('unlocked_inactive unlocked_active btn-info btn-default');
 }
@@ -31,3 +28,93 @@ $('#toggle_event_editing button').click(function () {
 })
 
 });
+
+
+    function SetCost() {
+        var data2 = parseInt($('#tsserver_time_payment').val());
+        var data1 = parseInt($("#tsserver_slots").val());
+        var money = UserMoney;
+        var data = data1 * 2 * data2;
+        if(data>money){
+            var lacks = "Нехватает: " + (data-money).toString() + "руб.";
+        }else{
+          var lacks = "";
+        }
+
+        $('span.cost').text(data);
+        $('span.lacks').text(lacks);
+}
+
+
+    $('#dns_input').bind('change', function (event) {
+      var dns = $('#dns_input').val();
+        if ((/^[A-Za-z0-9_-]+$/.test(dns)) && dns != '' ) {
+        $.ajax({
+            type: 'POST',
+            url: "/cabinet/free_dns",
+            data: { dns: dns },
+            success: function (data) {
+                if ((data.status) == true) {
+                    $('#dns_status').text('Домен занят');
+                    $('#dns_status').attr('style', 'color: red');
+                } else {
+                    $('#dns_status').text('Домен свободен');
+                    $('#dns_status').attr('style', 'color: green');
+                }
+            },
+            error: function (data) {
+
+            }
+
+        });
+    }else if (dns == ''){
+        $('#dns_status').text('Введите домен(необязательно)');
+    }else{
+        $('#dns_status').text('Недопустимый домен');
+    $('#dns_status').attr('style','color: red');
+}
+});
+
+
+
+    $('#tsserver_time_payment').bind('click', function(event){
+        SetCost();
+    });
+
+    $("#tsserver_slots").bind("keyup focusout", function(event){
+      var data = parseInt($("#tsserver_slots").val());
+    if (data>512)
+      {
+        $('#tsserver_slots').val(512);
+    data = 512;
+    SetCost();
+  }
+
+  if (data>=10)
+      {
+        var selector = $("[data-slider]");
+    selector.simpleSlider("setValue", data);
+    SetCost();
+
+  }
+
+});
+
+    $("#tsserver_slots").bind("focusout", function(event){
+      var data = parseInt($("#tsserver_slots").val());
+      if (data<10)
+      {
+        $('#tsserver_slots').val(10);
+    var selector = $("[data-slider]");
+    selector.simpleSlider("setValue", 10);
+    SetCost();
+
+  }
+});
+
+    $("[data-slider]").bind("slider:ready slider:changed", function (event, data) {
+        $('#tsserver_slots').val(data.value.toFixed(0));
+    SetCost();
+
+
+  });
