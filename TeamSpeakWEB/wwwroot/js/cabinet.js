b945fc27ad685782930d4f4ab752b87b5deb0f84
Pank <linkinpark02111@mail.ru>
2019-09-02 17:50:39 +0300

    fix js scripts for new Cabinet/New.cshtml

--- TeamSpeakWEB/wwwroot/js/cabinet.js
+++ TeamSpeakWEB/wwwroot/js/cabinet.js
@@ -1,22 +1,17 @@
-﻿
-
-$(document).ready({
-
-
-    function SetCost() {
-        var data2 = parseInt($('#tsserver_time_payment').val());
-        var data1 = parseInt($("#tsserver_slots").val());
-        var money = UserMoney;
-        var data = data1 * 2 * data2;
-        if(data > money) {
+﻿function SetCost() {
+    var data2 = parseInt($('#tsserver_time_payment').val());
+    var data1 = parseInt($("input#slots").val());
+    var money = UserMoney;
+    var data = data1 * 2 * data2;
+    if (data > money) {
         var lacks = "Нехватает: " + (data - money).toString() + "руб.";
-        } else {
-            var lacks = "";
-        }
+    } else {
+        var lacks = "";
+    }
 
-        $('span.cost').text(data);
-        $('span.lacks').text(lacks);
-    };
+    $('span.cost').text(data);
+    $('span.lacks').text(lacks);
+};
 
     $('#dns_input').bind('change', function (event) {
         var dns = $('#dns_input').val();
@@ -47,44 +42,10 @@ $(document).ready({
         }
     });
 
-
+    $('input#slots').bind('keyup focusout', function (event) {
+        SetCost();
+    });
 
     $('#tsserver_time_payment').bind('click', function (event) {
         SetCost();
     });
-
-    $("#tsserver_slots").bind("keyup focusout", function (event) {
-        var data = parseInt($("#tsserver_slots").val());
-        if (data > 512) {
-            $('#tsserver_slots').val(512);
-            data = 512;
-            SetCost();
-        }
-
-        if (data >= 10) {
-            var selector = $("[data-slider]");
-            selector.simpleSlider("setValue", data);
-            SetCost();
-
-        }
-
-    });
-
-    $("#tsserver_slots").bind("focusout", function (event) {
-        var data = parseInt($("#tsserver_slots").val());
-        if (data < 10) {
-            $('#tsserver_slots').val(10);
-            var selector = $("[data-slider]");
-            selector.simpleSlider("setValue", 10);
-            SetCost();
-
-        }
-    });
-
-    $("[data-slider]").bind("slider:ready slider:changed", function (event, data) {
-        $('#tsserver_slots').val(data.value.toFixed(0));
-        SetCost();
-    });
-
-
-});
