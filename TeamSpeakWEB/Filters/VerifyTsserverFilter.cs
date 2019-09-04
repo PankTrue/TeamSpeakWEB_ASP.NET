1f308fcf360a935e715447b19172f5aff9388494
Pank <linkinpark02111@mail.ru>
2019-09-04 21:52:44 +0300

    add filter for verify tsserver

---
+++ TeamSpeakWEB/Filters/VerifyTsserverFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeamSpeakWEB.Models;
+
+namespace TeamSpeakWEB.Filters
+{
+    public class VerifyTsserverFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            foreach (Tsserver arg in context.ActionArguments.Values.Where(v => v is Tsserver))
+            {
+                //TODO
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context) { }
+    }
+}
